15fd60ee596b31d6e646af0c752c8cfdc906d86d
MilenK <milen_kunchev@abv.bg>
2022-06-06 16:11:10 +0300

    update

--- petstagram/main/urls.py
+++ petstagram/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from petstagram.main.views import show_home
+from petstagram.main.views import show_home, show_dashboard, show_profile, show_pet_photo_details
 
 urlpatterns = (
     path('', show_home, name='index'),
-)
+    path('dashboard/', show_dashboard, name='dashboard'),
+    path('profile/', show_profile, name='profile'),
+    path('photo/details/<int:pk>/', show_pet_photo_details, name='pet photos details'),
 
+)
