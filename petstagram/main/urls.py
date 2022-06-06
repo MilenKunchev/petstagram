442bc19581e53ba837483825cb299b6eafe094f8
MilenK <milen_kunchev@abv.bg>
2022-06-06 17:21:22 +0300

    update

--- petstagram/main/urls.py
+++ petstagram/main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from petstagram.main.views import show_home, show_dashboard, show_profile, show_pet_photo_details
 
 urlpatterns = (
+
     path('', show_home, name='index'),
     path('dashboard/', show_dashboard, name='dashboard'),
     path('profile/', show_profile, name='profile'),
--- petstagram/main/views.py
+++ petstagram/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+""" рендерираме НОМЕ страницата за да я покажем
+view  е фунция която взима параметър  request  и връща HTP response"""
+
 
 def show_home(request):
     return render(request, 'home_page.html')
@@ -13,5 +16,5 @@ def show_profile(request):
     return render(request, 'profile_details.html')
 
 
-def show_pet_photo_details(request):
+def show_pet_photo_details(request, pk):
     return render(request, 'photo_details.html')
